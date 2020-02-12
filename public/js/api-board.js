5bbb2b6a542922192778585e5d9315038dc9a4c4
booldook <booldook@gmail.com>
2020-02-12 12:59:39 +0900

    api-board.html

--- public/js/api-board.js
+++ public/js/api-board.js
@@ -15,7 +15,7 @@ function onPopupClose() {
 /* 저장 로직 */
 $("#btSave").click(onSave);
 function onSave() {
-	var $f = $("#boardForm");
+	var $f = $("#writeForm");
 	var title = $f.find("input[name='title']").val();
 	var writer = $f.find("input[name='writer']").val();
 	var comment = $f.find("textarea[name='comment']").val();
@@ -31,6 +31,7 @@ function onSave() {
 		success: function(res) {
 			$(".popup-close").trigger("click");
 			$f[0].reset();
+			init();
 		},
 		error: err
 	});
@@ -59,7 +60,7 @@ function listMaker(res) {
 		html += '<td>'+res[i].rnum+'</td>';
 		html += '<td>';
 		html += '<button class="btn btn-danger btn-sm" onclick="onDelete('+res[i].id+');">삭제</button> ';
-		html += '<button class="btn btn-success btn-sm">수정</button>';
+		html += '<button class="btn btn-success btn-sm" onclick="onUpdate('+res[i].id+');">수정</button>';
 		html += '</td>';
 		html += '</tr>';
 	}
@@ -92,5 +93,47 @@ function onView(id) {
 			$("#popupView").stop().fadeIn(500);
 		},
 		err: err
+	});
+}
+
+/* 수정 로직 */
+function onUpdate(id) {
+	var $f = $("#updateForm");
+	$.ajax({
+		url: "/api/view/"+id,
+		type: "get",
+		success: function(res) {
+			$f.find("input[name='id']").val(res.id);
+			$f.find("input[name='title']").val(res.title);
+			$f.find("input[name='writer']").val(res.writer);
+			$f.find("textarea[name='comment']").val(res.comment);
+			$("#popupUpdate").stop().fadeIn(500);
+		},
+		err: err
+	});
+}
+$("#btUpdate").click(onChg);
+function onChg() {
+	var $f = $("#updateForm");
+	var id = $f.find("input[name='id']").val();
+	var title = $f.find("input[name='title']").val();
+	var writer = $f.find("input[name='writer']").val();
+	var comment = $f.find("textarea[name='comment']").val();
+	$.ajax({
+		url: "/api/update",
+		type: "post",
+		dataType: "json",
+		data: {
+			id: id,
+			title: title,
+			writer: writer,
+			comment: comment
+		},
+		success: function(res) {
+			$(".popup-close").trigger("click");
+			$f[0].reset();
+			init();
+		},
+		error: err
 	});
 }
